74d248629c203241af7d5de64408e840eedb7020
taem <taem@taemui-MacBookAir.local>
2025-07-09 14:54:02 +0900

    day10 score calculator init

---
+++ day10/score-calculator/score.js
@@ -0,0 +1,17 @@
+// 사용자 입력
+let inputStr = prompt("점수를 입력하세요.");
+console.log(inputStr);
+let input = parseInt(inputStr);
+console.log(input);
+
+const MAX_SCORE = 105;
+let score;
+var grade;
+
+// 최종점수 계산 (5점 추가)
+
+// 등급 결정 (if문)
+
+// 합격/불합격 여부 결정 (삼항연산자)
+
+// 등급에 따른 console.log() 출력 (switch문)
