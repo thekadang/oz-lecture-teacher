ef240a006258750501c0f082974ec1019c348116
taem <taem@taemui-MacBookAir.local>
2025-07-10 11:00:54 +0900

    day11 live coding1

---
+++ day11/commonfor.js
@@ -0,0 +1,17 @@
+// 0부터 4까지 콘솔찍는 코드
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log("//////////////////");
+
+// 1부터 5까지 콘솔찍는 코드
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+console.log("//////////////////");
+
+// 1부터 10까지 홀수만 찍는 코드
+for (let i = 1; i <= 10; i += 2) {
+  console.log(i);
+}
+console.log(i);
