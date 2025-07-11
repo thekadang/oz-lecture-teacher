64d5a5ac727695ac82468d099398a09b5681fb4e
taem <taem@taemui-MacBookAir.local>
2025-07-11 10:47:09 +0900

    object2

--- day12/object.js
+++ day12/object.js
@@ -14,7 +14,14 @@ const person = {
 console.log(person);
 
 // person의 age, isAdult 조회
+console.log("객체에서 . 사용하여 접근");
 let age = person.age;
 console.log("age :", age);
 let isAdult = person.isAdult ? "성인" : "미성년자";
 console.log("isAdult :", isAdult);
+
+console.log("객체에서 [key] 사용하여 접근");
+let age2 = person["age"];
+console.log("age2 :", age2);
+let isAdult2 = person["isAdult"] ? "성인" : "미성년자";
+console.log("isAdult2 :", isAdult2);
