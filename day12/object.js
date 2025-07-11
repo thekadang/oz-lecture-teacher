b3a6cd52e9fb6289f259e4089e73d8c954e5d000
taem <taem@taemui-MacBookAir.local>
2025-07-11 10:40:58 +0900

    object init

--- day12/object.js
+++ day12/object.js
@@ -1,1 +1,14 @@
 console.log("객체 실습");
+
+const person = {
+  name: {
+    firstName: "Gildong",
+    lastName: "Hong",
+  },
+  age: 20,
+  isAdult: true,
+  printInfo: function () {
+    console.log("printInfo");
+  },
+};
+console.log(person);
