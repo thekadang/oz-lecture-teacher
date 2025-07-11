be7e6d75aef486d41ca968f0a1594c7d12bc1a01
taem <taem@taemui-MacBookAir.local>
2025-07-11 13:33:43 +0900

    filter

--- day12/functional.js
+++ day12/functional.js
@@ -21,7 +21,38 @@
 // console.log(numbers);
 
 let numbers = [1, 2, 3];
-numbers = numbers.map((num) => {
-  return num * 2;
-});
-console.log(numbers);
+// numbers = numbers.map((num) => {
+//   return num * 2;
+// });
+
+numbers = numbers.map((num) => num * 2);
+// console.log(numbers);
+
+// filter
+let numbersA = [1, 2, 3, 4, 5, 6];
+// 짝수인 값만 빼고 싶어요
+
+// let newNumbersA = [];
+// for (const num of numbersA) {
+//   if (num % 2 === 0) newNumbersA.push(num);
+// }
+// console.log(newNumbersA);
+
+// const even = function (num) {
+//   if (num % 2 === 0) return num;
+// };
+// let newNumbersA = [];
+// for (const num of numbersA) {
+//   if (even(num)) newNumbersA.push(even(num));
+// }
+// console.log(newNumbersA);
+
+// numbersA = numbersA.filter((num) => {
+//   return num % 2 === 0;
+// });
+
+numbersA = numbersA.filter((num) => num % 2 === 0);
+console.log(numbersA);
+
+// reduce
+let numberB = [1, 2, 3, 4];
