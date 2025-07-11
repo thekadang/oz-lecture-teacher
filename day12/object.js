c4d0970c497bfcfb4316495c5c3c4ca261ad61f0
taem <taem@taemui-MacBookAir.local>
2025-07-11 10:56:54 +0900

    object3

--- day12/object.js
+++ day12/object.js
@@ -14,7 +14,7 @@ const person = {
 console.log(person);
 
 // person의 age, isAdult 조회
-console.log("객체에서 . 사용하여 접근");
+console.log("객체에서 . 사용하여 접근"); // 객체에서는 이 방법을 주로 사용
 let age = person.age;
 console.log("age :", age);
 let isAdult = person.isAdult ? "성인" : "미성년자";
@@ -25,3 +25,17 @@ let age2 = person["age"];
 console.log("age2 :", age2);
 let isAdult2 = person["isAdult"] ? "성인" : "미성년자";
 console.log("isAdult2 :", isAdult2);
+
+// 객체에 키가 뭐가 왔는지 모르지만 어쨌든 모든 값을 꺼내고 싶을 때. []를 사용한다.
+let keys = [];
+for (const key in person) {
+  keys.push(key);
+}
+// console.log(keys);
+// for (const key of keys) {
+//   console.log("key", key);
+//   console.log("person[key]", person[key]);
+// }
+
+// 객체 내의 함수 사용하기
+person.printInfo();
