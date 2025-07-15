2daf4f1509bbb5a62f534d5b9df794cf70d34ff1
taem <taem@taemui-MacBookAir.local>
2025-07-15 13:22:47 +0900

    day14 async fix error

--- day14/async/async.js
+++ day14/async/async.js
@@ -2,7 +2,7 @@ async function fetchData() {
   let promise = new Promise((resolve) =>
     setTimeout(() => resolve("데이터 가져옴!"), 1000)
   );
-  let result = await promise();
+  let result = await promise;
   console.log(result);
 }
 fetchData();
