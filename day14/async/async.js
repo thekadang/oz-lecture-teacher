e106b1974d6d0364ec16f708e9268982ee497360
taem <taem@taemui-MacBookAir.local>
2025-07-15 13:24:12 +0900

    day14 fetchData

--- day14/async/async.js
+++ day14/async/async.js
@@ -1,8 +1,12 @@
 async function fetchData() {
-  let promise = new Promise((resolve) =>
-    setTimeout(() => resolve("데이터 가져옴!"), 1000)
-  );
-  let result = await promise;
-  console.log(result);
+  try {
+    let promise = new Promise((resolve) =>
+      setTimeout(() => resolve("데이터 가져옴!"), 1000)
+    );
+    let result = await promise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
 fetchData();
