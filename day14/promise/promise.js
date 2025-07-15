072b5db1317b1684b7f55baf70818d38f5b95a53
taem <taem@taemui-MacBookAir.local>
2025-07-15 12:57:55 +0900

    promise all

--- day14/promise/promise.js
+++ day14/promise/promise.js
@@ -19,6 +19,6 @@ promise
   });
 
 // Promise.all 병렬처리
-const p1 = new Promise((resolve) => setTimeout(() => resolve("작업1"), 1000));
-const p2 = new Promise((resolve) => setTimeout(() => resolve("작업2"), 2000));
+const p1 = new Promise((resolve) => setTimeout(() => resolve("작업1"), 5000));
+const p2 = new Promise((resolve) => setTimeout(() => resolve("작업2"), 5000));
 Promise.all([p1, p2]).then((results) => console.log(results));
