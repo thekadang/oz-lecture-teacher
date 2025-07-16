3af4f1ec820926ea5db6ce2fac8cc73fcf803e4a
taem <taem@taemui-MacBookAir.local>
2025-07-16 11:14:10 +0900

    json stringify

--- day15/json/json.js
+++ day15/json/json.js
@@ -48,3 +48,15 @@ console.log(namesMap);
 // Array.map + spread 연산자
 const namesMapSp = users.map(({ name }) => name);
 console.log(namesMapSp);
+
+// JSON.stringify()
+const namesMapString = JSON.stringify(namesMap);
+console.log(namesMapString);
+const jsonString3 = `{
+    "name": "지민",
+    "age": 22
+}`;
+const jsonObj3 = JSON.parse(jsonString3);
+console.log("jsonObj3", jsonObj3);
+const jsonObj3String = JSON.stringify(jsonObj3);
+console.log("jsonObj3String", jsonObj3String);
