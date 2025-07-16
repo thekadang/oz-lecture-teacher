f60d5cb2216a365677ea2380d6ab33fd66aa9d5e
taem <taem@taemui-MacBookAir.local>
2025-07-16 11:39:25 +0900

    day15 exception error

--- day15/exception/network.js
+++ day15/exception/network.js
@@ -1,10 +1,14 @@
-async function getPost() {
+async function getPost(postNum) {
   try {
-    let response = await fetch("https://jsonplaceholder.typicode.com/posts/2");
-    let data = await response.json();
-    console.log(data);
+    let response = await fetch(
+      `https://jsonplaceholderfdfdfdfdfdf.typicode.com/posts/${postNum}`
+    );
+    console.log(response);
+    // let data = await response.json();
+    // console.log(data);
   } catch (error) {
+    console.log(error);
     alert("에러: " + error.message);
   }
 }
-getPost();
+getPost("fdfd");
