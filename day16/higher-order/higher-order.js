fa8cac7142509f361399ae9c1558bb2debd95e6e
taem <taem@taemui-MacBookAir.local>
2025-07-17 12:59:21 +0900

    day16 higher-order

---
+++ day16/higher-order/higher-order.js
@@ -0,0 +1,23 @@
+// Array.map()
+// 배열을 다른 배열로 매핑할 때 사용
+
+let movies = [
+  {
+    title: "미션 임파서블:파이널 레코닝",
+    director: "크리스토퍼 맥쿼리",
+    year: 2025,
+    genre: "Action",
+  },
+  {
+    title: "파묘",
+    director: "장재현",
+    year: 2024,
+    genre: "Mistery",
+  },
+  {
+    title: "서울의 봄",
+    director: "김성수",
+    year: 2023,
+    genre: "Drama",
+  },
+];
