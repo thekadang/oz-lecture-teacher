22f98c694b0a5e65d3d1057825d99d4f97c13d0d
taem <taem@taemui-MacBookAir.local>
2025-07-17 11:11:07 +0900

    day16 functional2

--- day16/functional/functional.js
+++ day16/functional/functional.js
@@ -23,25 +23,35 @@
 // let a = addTotal(add(1, 2));
 // console.log(a);
 
-const add = function (a) {
-  return function (b) {
-    return a + b;
-  };
-};
-
-const add5 = add(5);
-const add5and10 = add5(10);
-
-// const add = function (a, b) {
-//   return a + b;
+// const add = function (a) {
+//   return function (b) {
+//     return a + b;
+//   };
 // };
-// console.log(add(2, 3)); // 5
 
-// function addTotal(add1, add2) {
-//   return add(add1, add2);
-// }
-// const total = addTotal(add(1, 2), add(3, 4));
-// console.log(total);
+// const add5 = add(5);
+// const add5and10 = add5(10);
+
+const add = function (a, b) {
+  return a + b;
+};
+console.log(add(2, 3)); // 5
+
+function cal(func, num1, num2) {
+  return func(num1, num2);
+}
+console.log(cal(add, 2, 3));
+
+function addTotal(add1, add2) {
+  return add1(add2(1, 2), 3);
+}
+const total = addTotal(add, add);
+console.log(total);
+
+function addTotalMistake(add1, add2) {
+  return add(add1, add2);
+}
+console.log(addTotalMistake(add(1, 2), add(3, 4)));
 {
   /* <button onclick="handleClick()"> </button> */
 }
