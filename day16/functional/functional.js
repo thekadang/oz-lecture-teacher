5979132a3a13845de412c0acb34d5c85dc2f08e4
taem <taem@taemui-MacBookAir.local>
2025-07-17 11:00:56 +0900

    day16 functional

---
+++ day16/functional/functional.js
@@ -0,0 +1,47 @@
+// function add(a, b) {
+//   return a + b;
+// }
+// const result1 = add(2, 3); //5
+// const result2 = add("나는", "태민");
+// const result3 = add(true, false);
+
+// let num1 = 2;
+// let num2 = 3;
+// const result4 = add(num1, num2); // 인수
+
+// const add = function (a, b) {
+//   return a + b;
+// };
+
+// const add = (a, b) => a + b;
+
+// function addTotal(add1) {
+//   return add1;
+// }
+
+// addTotal(1);
+// let a = addTotal(add(1, 2));
+// console.log(a);
+
+const add = function (a) {
+  return function (b) {
+    return a + b;
+  };
+};
+
+const add5 = add(5);
+const add5and10 = add5(10);
+
+// const add = function (a, b) {
+//   return a + b;
+// };
+// console.log(add(2, 3)); // 5
+
+// function addTotal(add1, add2) {
+//   return add(add1, add2);
+// }
+// const total = addTotal(add(1, 2), add(3, 4));
+// console.log(total);
+{
+  /* <button onclick="handleClick()"> </button> */
+}
