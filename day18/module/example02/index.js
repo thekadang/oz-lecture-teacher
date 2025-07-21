6328828b72576415a01ae7a0c287abb42fec1d5e
taem <taem@taemui-MacBookAir.local>
2025-07-21 10:51:21 +0900

    day18 module05

--- day18/module/example02/index.js
+++ day18/module/example02/index.js
@@ -1,3 +1,12 @@
-import { title as printTitle, print as pt } from "./printer.js";
+import { title as printTitle, print } from "./printer.js";
+import { title as scannerTitle, scanner } from "./scanner.js";
 
-pt(printTitle);
+const userValue = "스캔해야할 값";
+
+// 스캔해야할 값을 스캔하고
+const scanValue = scanner(userValue);
+console.log("scanner title : ", scannerTitle);
+
+// 스캔한 결과를 프린트한다.
+print(scanValue);
+console.log("printer title : ", printTitle);
